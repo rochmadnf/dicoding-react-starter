f77a23a5058542f1a3cac444b3df3988ee089e31
rochmadnf <rochmadnf@gmail.com>
2022-07-06 15:11:33 +0800

    styling ui dengan css modular

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import ContactApp from './pages/ContactApp';
 
+/* styling */
+import './style.css'
 
 createRoot(document.getElementById('root')).render(<ContactApp />)
